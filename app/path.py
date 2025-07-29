1202c095e63086ba273026058287b156a4782394
jonel@DAS-IT <jonel@DAS-IT>
2025-07-29 15:55:27 +0200

    add add app/path.py  and update app/plot.py cmd.txt

--- app/path.py
+++ app/path.py
@@ -8,3 +8,43 @@ chickens_path = 'https://assets.datacamp.com/production/repositories/2409/datase
 # Update the path to where you unzipped the shapefile
 #shapefile_path = "data/ne_110m_admin_0_countries/ne_110m_admin_0_countries.shp"
 shapefile_path = "data/maps/ne_110m_admin_0_countries.shp"
+
+#import geopandas as gpd
+#gadm41_MOZ_3.shp
+#maputo = gpd.read_file("data/maps/maputo_admin.shp")
+#maputo = gpd.read_file("data/maps/gadm41_MOZ_3.shp")
+maputo_path = "data/maps/gadm41_MOZ_3.shp"
+
+#maputo = gpd.read_file(maputo_path)
+#maputo_path = "data/maps/gadm41_MOZ_3.shp"
+
+#data/maps/ne_110m_admin_0_countries.shp
+#maputo = gpd.read_file("data/maps/ne_110m_admin_0_countries.shp")
+#inflating: data/maps/gadm41_MOZ_0.shp
+#  inflating: data/maps/gadm41_MOZ_1.shp
+# inflating: data/maps/gadm41_MOZ_2.shp
+# inflating: data/maps/gadm41_MOZ_3.shp 
+# Limite de Maputo
+
+# Vias de acesso
+roads_path_ = "data/maps/gis_osm_roads_free_1.shp"
+
+# Hospitais em Maputo
+#hospitais = gpd.read_file("data/maps/hospitais_maputo.shp")
+# gadm41_MOZ_1.shp, gadm41_MOZ_2.shp, gadm41_MOZ_3.shp, ne_110m_admin_1_states_provinces.shp
+#pyogrio.errors.DataSourceError: data/maps/hospitais_maputo.shp: No such file or directory
+
+
+#roads = gpd.read_file("data/maps/gis_osm_roads_free_1.shp")
+
+
+#hospitais = gpd.read_file("data/maps/gadm41_MOZ_0.shp") #gadm41_MOZ_1.shp, gadm41_MOZ_2.shp, gadm41_MOZ_3.shp, ne_110m_admin_1_states_provinces.shp, 
+
+hospitais_path = "data/maps/gadm41_MOZ_0.shp" #gpd.read_file("data/maps/gadm41_MOZ_0.shp") #gadm41_MOZ_1.shp, gadm41_MOZ_2.shp, gadm41_MOZ_3.shp, ne_110m_admin_1_states_provinces.shp, 
+
+
+#inflating: data/maps/gis_osm_buildings_a_free_1.shp, inflating: data/maps/gis_osm_landuse_a_free_1.shp, data/maps/gis_osm_natural_a_free_1.shp , data/maps/gis_osm_natural_free_1.shp, 
+# data/maps/gis_osm_places_a_free_1.shp, data/maps/gis_osm_places_free_1.shp, data/maps/gis_osm_pofw_a_free_1.shp , data/maps/gis_osm_pofw_free_1.shp, data/maps/gis_osm_pois_a_free_1.shp
+# data/maps/gis_osm_pois_free_1.shp, data/maps/gis_osm_railways_free_1.shp, data/maps/gis_osm_roads_free_1.shp, data/maps/gis_osm_traffic_a_free_1.shp
+# data/maps/gis_osm_traffic_free_1.shp, data/maps/gis_osm_transport_a_free_1.shp, data/maps/gis_osm_transport_free_1.shp , 
+# data/maps/gis_osm_water_a_free_1.shp, data/maps/gis_osm_waterways_free_1.shp
--- app/plot.py
+++ app/plot.py
@@ -4,13 +4,21 @@ import pandas as pd
 import geopandas as gpd
 import matplotlib.pyplot as plt
 
-from path import path_file, chickens_path, shapefile_path
+from path import path_file, chickens_path, shapefile_path, hospitais_path, roads_path_, maputo_path
+
 from helper_function import create_scatterplot
 
 schools = pd.read_csv(path_file)  # Make sure this file exists and has Longitude/Latitude columns
 chickens = pd.read_csv(chickens_path)
 
-print(schools.columns)
+# 1.3 
+maputo = gpd.read_file(maputo_path)
+
+roads = gpd.read_file(roads_path_)
+
+hospitais = gpd.read_file(hospitais_path)  #gadm41_MOZ_1.shp, gadm41_MOZ_2.shp, gadm41_MOZ_3.shp, ne_110m_admin_1_states_provinces.shp, 
+
+#print(schools.columns)
 
 ## Scatterplot 1 - father heights vs. son heights with darkred square markers
 # como usar um helper para plotar scatterplot
@@ -21,7 +29,7 @@ create_scatterplot(schools.Longitude, schools.Latitude, color='darkred', marker=
 #plt.scatter(schools.Longitude, schools.Latitude, c='darkgreen', marker='s') #'p')
 
 # print the first few rows of df 
-print(schools.head())
+#print(schools.head())
 
 # extract latitude to a new column: lat
 # df['lat'] = [loc[0] for loc in df.Location]
@@ -39,7 +47,7 @@ print(schools.head())
 #plt.show() # Show your plot
 
 # Look at the first few rows of the chickens DataFrame
-print(chickens.head())
+#print(chickens.head())
 
 # Plot the locations of all Nashville chicken permits
 #plt.scatter(x = chickens.lat, y = chickens.lng)
@@ -49,24 +57,30 @@ create_scatterplot(chickens.lng, chickens.lat,  color='darkred', marker='p', xla
 
 # Show the plot
 #plt.show()
-print(" import geopandas")
+#print(" import geopandas")
 
 print("world = gpd.read_file(gpd.datasets.get_path(")
 # Load the world map
 
 
 world = gpd.read_file(shapefile_path)
-mozambique = world[world['ADMIN'] == 'Mozambique']
-print("mozambique = world[world['admin'] == 'Mozambique']")
-print("mozambique = world[world.admin ==")
-# Plot
-mozambique.plot(edgecolor='black', color='lightblue')
-plt.title("Mozambique - Geometry Map")
-#plt.show()
 
-plt.savefig("img/1_Building_2_Layer_Maps/Mozambique_Geometry_Map.png")  # Save the plot as a PNG image
-print("plt.savefig(")
-#plt.show() # Show your plot
+def print_world_info(world, moz='Mozambique', title_="Mozambique - Geometry Map", file_name_="img/1_Building_2_Layer_Maps/2._1_world_Building_2_Layer_Maps_Mozambique_Geometry_Map.png"):
+    
+    print(world.head())  # Print the first few rows of the world GeoDataFrame
+    print(world.columns)  # Print the columns of the world GeoDataFrame 
+    
+    mozambique = world[world['ADMIN'] == moz]
+    #print("mozambique = world[world['admin'] == 'Mozambique']")
+    #print("mozambique = world[world.admin ==")
+    # Plot
+    mozambique.plot(edgecolor='black', color='lightblue')
+    plt.title(title_)
+    #plt.show()
+
+    plt.savefig(file_name_)  # Save the plot as a PNG image
+    print("plt.savefig saved ")
+    #plt.show() # Show your plot
 
 # https://campus.datacamp.com/courses/visualizing-geospatial-data-in-python/building-2-layer-maps-combining-polygons-and-scatterplots?ex=7
 #1.2.1 Creating a GeoDataFrame & examining the geometry
@@ -91,29 +105,3 @@ plt.savefig("img/1_Building_2_Layer_Maps/2._1_service_district_Building_2_Layer_
 plt.close()  # Fecha a figura para liberar memória (opcional)
 
 
-#import geopandas as gpd
-#gadm41_MOZ_3.shp
-#maputo = gpd.read_file("data/maps/maputo_admin.shp")
-maputo = gpd.read_file("data/maps/gadm41_MOZ_3.shp")
-
-#data/maps/ne_110m_admin_0_countries.shp
-#maputo = gpd.read_file("data/maps/ne_110m_admin_0_countries.shp")
-#inflating: data/maps/gadm41_MOZ_0.shp
-#  inflating: data/maps/gadm41_MOZ_1.shp
-# inflating: data/maps/gadm41_MOZ_2.shp
-# inflating: data/maps/gadm41_MOZ_3.shp 
-# Limite de Maputo
-
-# Vias de acesso
-roads = gpd.read_file("data/maps/gis_osm_roads_free_1.shp")
-
-# Hospitais em Maputo
-#hospitais = gpd.read_file("data/maps/hospitais_maputo.shp")
-# gadm41_MOZ_1.shp, gadm41_MOZ_2.shp, gadm41_MOZ_3.shp, ne_110m_admin_1_states_provinces.shp
-#pyogrio.errors.DataSourceError: data/maps/hospitais_maputo.shp: No such file or directory
-hospitais = gpd.read_file("data/maps/gadm41_MOZ_0.shp") #gadm41_MOZ_1.shp, gadm41_MOZ_2.shp, gadm41_MOZ_3.shp, ne_110m_admin_1_states_provinces.shp, 
-#inflating: data/maps/gis_osm_buildings_a_free_1.shp, inflating: data/maps/gis_osm_landuse_a_free_1.shp, data/maps/gis_osm_natural_a_free_1.shp , data/maps/gis_osm_natural_free_1.shp, 
-# data/maps/gis_osm_places_a_free_1.shp, data/maps/gis_osm_places_free_1.shp, data/maps/gis_osm_pofw_a_free_1.shp , data/maps/gis_osm_pofw_free_1.shp, data/maps/gis_osm_pois_a_free_1.shp
-# data/maps/gis_osm_pois_free_1.shp, data/maps/gis_osm_railways_free_1.shp, data/maps/gis_osm_roads_free_1.shp, data/maps/gis_osm_traffic_a_free_1.shp
-# data/maps/gis_osm_traffic_free_1.shp, data/maps/gis_osm_transport_a_free_1.shp, data/maps/gis_osm_transport_free_1.shp , 
-# data/maps/gis_osm_water_a_free_1.shp, data/maps/gis_osm_waterways_free_1.shp
