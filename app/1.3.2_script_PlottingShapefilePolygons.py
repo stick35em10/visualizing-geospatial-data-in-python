d1954b14a00b1545b4623a8f7a2b85c3ee45700b
jonel@DAS-IT <jonel@DAS-IT>
2025-08-15 16:16:38 +0200

    15.08: 16'16  update .github/workflows/01_03_workflows.yml add app/helper_function.py

---
+++ app/1.3.2_script_PlottingShapefilePolygons.py
@@ -0,0 +1,17 @@
+
+# Import packages
+import geopandas as gpd
+from path import service_district #, world, maputo_, roads, hospitais  # Import the necessary paths
+import matplotlib.pyplot as plt
+
+# Plot the Service Districts without any additional arguments
+#service_district.plot()
+#plt.show()
+
+# Plot the Service Districts, color them according to name, and show a legend
+service_district.plot(column = 'name', legend = True)
+#plt.show()
+#file_naimg/1_Building_2_Layer_Maps/1.3.1_Geometry/_2.PlottingShapefil
+#ePpolygons.png
+img_file_name = "img/1_Building_2_Layer_Maps/1.3.1_Geometry/_2.PlottingShapefilePpolygons.png"
+plt.savefig(img_file_name_)
